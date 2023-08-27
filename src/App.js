4e8fc92b3d51e322d7655e647ffa1633468d5bd6
Shashwat Adhau <shashwatadhau3@gmail.com>
2023-08-27 14:08:10 +0530

    adding frind to list

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialFriends = [
   {
     id: 118836,
@@ -20,17 +22,33 @@ const initialFriends = [
 ];
 
 const App = () => {
+  const [friends, setFriends] = useState(initialFriends);
+  const [showAddFriend, setShowAddFriend] = useState(false);
+
+  const addFriendHandler = () => {
+    setShowAddFriend((pre) => !pre);
+  };
+
+  const handleAddFriend = (friend) => {
+    setFriends((pre) => [...pre, friend]);
+  };
+
   return (
     <div className="app">
       <div className="sidebar">
-        <FriendsList />
+        <FriendsList friends={friends} />
+        {showAddFriend && <FormAddFriend onAddFriend={handleAddFriend} />}
+        <Button onClick={addFriendHandler}>
+          {showAddFriend ? "Close" : "Add Friend"}
+        </Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 };
 
-const FriendsList = () => {
-  const friends = initialFriends;
+const FriendsList = ({ friends }) => {
+  // const friends = initialFriends;
   return (
     <ul>
       {friends.map((friend) => (
@@ -56,8 +74,72 @@ const Friend = ({ friend }) => {
         </p>
       )}
       {friend.balance === 0 && <p>You and {friend.name} are even</p>}
-      <button className="button"> Select</button>
+      <Button>Select</Button>
     </li>
+  );
+};
+
+const FormAddFriend = ({ onAddFriend }) => {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("https://i.pravatar.cc/48");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name || !image) return;
+
+    const id = crypto.randomUUID();
+    const newFriend = {
+      name,
+      image: `${image}?={id}`,
+      balance: 0,
+      id: id,
+    };
+    onAddFriend(newFriend);
+    setName("");
+    setImage("https://i.pravatar.cc/48");
+  };
+  return (
+    <form className="form-add-friend" onSubmit={handleSubmit}>
+      <label>🙎 Friend Name</label>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <label>📷Image URL</label>
+      <input
+        type="text"
+        value={image}
+        onChange={(e) => setImage(e.target.value)}
+      />
+      <Button>Add</Button>
+    </form>
+  );
+};
+
+const FormSplitBill = () => {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+      <label>💰 Bill Value</label>
+      <input type="text" />
+
+      <label>🧍‍♀️ Your Expense</label>
+      <input type="text" />
+
+      <label>👯‍♀️ X's Value</label>
+      <input type="text" />
+      <Button>Add</Button>
+    </form>
+  );
+};
+
+const Button = ({ onClick, children }) => {
+  return (
+    <button onClick={onClick} className="button">
+      {children}
+    </button>
   );
 };
 
